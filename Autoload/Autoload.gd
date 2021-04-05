e3e0bc4be5386778ce5095aa6d39d5cf07a85cb3
Rafał Mikrut <mikrutrafal@protonmail.com>
2021-04-05 16:55:41 +0200

    Move Physics3D at the end

--- Autoload/Autoload.gd
+++ Autoload/Autoload.gd
@@ -17,10 +17,10 @@ const alone_steps : Array = [
 	"res://Nodes/Nodes.tscn",
 	"res://ReparentingDeleting/ReparentingDeleting.tscn",
 	"res://Physics/2D/Physics2D.tscn",
-	"res://Physics/3D/Physics3D.tscn",
 	"res://Rendering/Lights2D/Lights2D.tscn",
 	"res://Rendering/Lights3D/Lights3D.tscn",
 	"res://AutomaticBugs/FunctionExecutor.tscn", # Only Needs to be executed once, but this is workaround a little
+	"res://Physics/3D/Physics3D.tscn", # Loads very long but isn't necessarry so we skip it now
 ]
 
 # All scenes run in one step
